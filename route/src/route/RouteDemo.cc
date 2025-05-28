9d55d744ab9f563735c91137d8bb17b0a3547c81
HatCun <142104320+gr32ey20es@users.noreply.github.com>
2025-05-28 22:01:51 +0700

    Basic structure

---
+++ route/src/route/RouteDemo.cc
@@ -0,0 +1,57 @@
+#include "route/RouteDemo.h"
+#include "route/RouteDemoMessage_m.h"
+
+using namespace veins;
+using namespace route;
+
+Define_Module(route::RouteDemo);
+
+void RouteDemo::initialize(int stage)
+{
+    DemoBaseApplLayer::initialize(stage);
+    if (stage == 0) {
+        // Initializing members and pointers of your application goes here
+        EV << "Initializing " << par("appName").stringValue() << std::endl;
+    }
+    else if (stage == 1) {
+        // Initializing members that require initialized other modules goes here
+    }
+}
+
+void RouteDemo::finish()
+{
+    DemoBaseApplLayer::finish();
+    // statistics recording goes here
+}
+
+void RouteDemo::onBSM(DemoSafetyMessage* bsm)
+{
+    // Your application has received a beacon message from another car or RSU
+    // code for handling the message goes here
+}
+
+void RouteDemo::onWSM(BaseFrame1609_4* wsm)
+{
+    // Your application has received a data message from another car or RSU
+    // code for handling the message goes here, see TraciDemo11p.cc for examples
+}
+
+void RouteDemo::onWSA(DemoServiceAdvertisment* wsa)
+{
+    // Your application has received a service advertisement from another car or RSU
+    // code for handling the message goes here, see TraciDemo11p.cc for examples
+}
+
+void RouteDemo::handleSelfMsg(cMessage* msg)
+{
+    DemoBaseApplLayer::handleSelfMsg(msg);
+    // this method is for self messages (mostly timers)
+    // it is important to call the DemoBaseApplLayer function for BSM and WSM transmission
+}
+
+void RouteDemo::handlePositionUpdate(cObject* obj)
+{
+    DemoBaseApplLayer::handlePositionUpdate(obj);
+    // the vehicle has moved. Code that reacts to new positions goes here.
+    // member variables such as currentPosition and currentSpeed are updated in the parent class
+}
---
+++ route/src/route/RouteDemo.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "route/route.h"
+#include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
+
+using namespace veins;
+namespace route { class RouteDemo; }
+
+class ROUTE_API route::RouteDemo : public veins::DemoBaseApplLayer
+{
+public:
+    void initialize(int stage) override;
+    void finish() override;
+
+protected:
+    void onBSM(DemoSafetyMessage* bsm) override;
+    void onWSM(BaseFrame1609_4* wsm) override;
+    void onWSA(DemoServiceAdvertisment* wsa) override;
+
+    void handleSelfMsg(cMessage* msg) override;
+    void handlePositionUpdate(cObject* obj) override;
+};
